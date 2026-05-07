9cc117a742dd4fd405eabe456d297e3f9dc88b75
Tim_M <tmerk355@gmail.com>
2026-05-07 21:10:38 +0200

    Removed playground

--- src/QuantumLibrary.Playground/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using QuantumWaves;
-using QuantumWaves.Solutions;
-
-WaveFunction1D infiniteWell = HarmonicOscillator1D.State(3, 1f, 1f);
-Console.WriteLine($"True normalization constant: {infiniteWell.Amplitude}");
-
-Console.WriteLine(infiniteWell.TryNormalize());
-Console.WriteLine($"Approx normalization constant: {infiniteWell.Amplitude}");
